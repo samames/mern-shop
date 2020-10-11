bdc12664300212b5ed361c74e6e908e6a5dc1e80
sam ames <samames@gmail.com>
2020-10-12 00:58:25 +0100

    added some models

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 import express from 'express';
+import connectDB from './config/db.js';
 import dotenv from 'dotenv';
+import colors from 'colors';
 import products from './data/products.js';
 dotenv.config();
 const app = express();
+
+connectDB();
 
 app.get('/', (req, res) => {
   res.send('API is running');
@@ -21,5 +25,7 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(
   PORT,
-  console.log(`server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(
+    `server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
 );
