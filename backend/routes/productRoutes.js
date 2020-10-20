d729481508a294b047c7fd45d0ccade825e6f819
sam ames <samames@gmail.com>
2020-10-21 00:20:14 +0100

    register user route ctrl

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,28 +1,12 @@
 import express from 'express';
+import {
+  getProducts,
+  getProductById,
+} from '../controllers/productController.js';
 const router = express.Router();
-import handler from 'express-async-handler';
-import Product from '../models/productModel.js';
 
-router.get(
-  '/',
-  handler(async (req, res) => {
-    const products = await Product.find({});
+router.route('/').get(getProducts);
 
-    res.json(products);
-  })
-);
-
-router.get(
-  '/:id',
-  handler(async (req, res) => {
-    const product = await Product.findById(req.params.id);
-    if (product) {
-      res.json(product);
-    } else {
-      res.status(404);
-      throw new Error('Product not found');
-    }
-  })
-);
+router.route('/:id').get(getProductById);
 
 export default router;
---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import {
+  authUser,
+  registerUser,
+  getUserProfile,
+} from '../controllers/userController.js';
+import { protect } from '../middleware/authMiddleware.js';
+const router = express.Router();
+
+router.route('/').post(registerUser);
+router.route('/profile').get(protect, getUserProfile);
+router.post('/login', authUser);
+
+export default router;
